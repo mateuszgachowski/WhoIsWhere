7a1596750e461a72864ddf9a6453ea5fc50f6f3e
Mateusz Gachowski <mateusz.gachowski@gmail.com>
2014-10-09 23:53:13 +0200

    new: license baner added to js files

--- app/controllers/desk.js
+++ app/controllers/desk.js
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2014 Mateusz Gachowski, contributors.
+ * Licensed under the MIT license (see LICENSE file).
+ */
 (function () {
   'use strict';
 
--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,6 +1,9 @@
+/**
+ * Copyright (c) 2014 Mateusz Gachowski, contributors.
+ * Licensed under the MIT license (see LICENSE file).
+ */
 (function () {
   'use strict';
-
 
   App.controller('ProfileController', ['$scope', function ($scope) {
     
